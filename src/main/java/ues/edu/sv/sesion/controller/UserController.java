9c12b4aa754f94763674c750fcb69d1eb794abf7
Alejandro Aragon <ar17092@ues.edu.sv>
2022-09-25 22:52:24 -0600

    mostrando una plantilla html

--- src/main/java/ues/edu/sv/sesion/controller/UserController.java
+++ src/main/java/ues/edu/sv/sesion/controller/UserController.java
@@ -2,12 +2,14 @@ package ues.edu.sv.sesion.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import ues.edu.sv.sesion.service.UserService;
 
-http://localhost:8080/user
+
+//http://localhost:8080/user/all-user
 @Controller
 @RequestMapping(value = "user")
 public class UserController {
@@ -15,7 +17,11 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @GetMapping(value = "/")
-    public String 
+    @GetMapping(value="/all-user")
+    public String getAllUsersView(Model model) {
+        model.addAttribute("user-list",userService.getAllUser());
+        return "index.html";//devolviendo el template html
+    }
+    
     
 }
